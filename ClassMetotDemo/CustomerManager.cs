b4bc9f8d4251874d84634d6771c82fd297ecb83b
meltem14 <m.ataman14@outlook.com>
2021-02-21 23:59:51 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " kişisi eklendi");
+        }
+
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " kişisi silindi");
+        }
+
+        public void List(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Ad & Soyad: " + customer.Name + customer.Surname);
+                Console.WriteLine("Yaş: " + customer.Age);
+                Console.WriteLine("------------");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Name = "Ahmet";
+            customer1.Surname = "Şen";
+            customer1.Age = 20;
+            customer1.CustomerId = 1;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Hüseyin";
+            customer2.Surname = "Şen";
+            customer2.Age = 23;
+            customer2.CustomerId = 2;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            Console.WriteLine("-----------");
+            customerManager.Remove(customer2);
+            Console.WriteLine("-----------");
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            Console.WriteLine("Müşteri Listesi");
+            customerManager.List(customers);
+        }
+    }
+}
